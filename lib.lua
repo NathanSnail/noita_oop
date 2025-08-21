a76d8aadc3b3897078885dacf8e3bda4144b1fd4
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-08-22 02:40:23 +1200

    forgot to commit file?

--- lib.lua
+++ lib.lua
@@ -1,54 +1,19 @@
-if not TESTING then
-	---@type string
-	local whoami = MODID
-	if not MODID then
-		local path = "data/noita_oop/temp.txt"
-		ModTextFileSetContent(path, "empty")
-		whoami = ModTextFileWhoSetContent(path)
-	end
-
-	---@param modname string
-	---@return any
-	function require(modname)
-		return dofile_once(("mods/%s/lib/ecs/%s.lua"):format(whoami, (modname:gsub("%.", "/"))))
-	end
+---@type string
+local whoami = MODID
+if not MODID then
+	local path = "data/noita_oop/temp.txt"
+	ModTextFileSetContent(path, "empty")
+	whoami = ModTextFileWhoSetContent(path)
 end
 
-local Entity = require "src.Entity"
-local freeze = require "src.freeze"
-local metatable = require "src.metatable"
-local typed = require "src.typed"
+local _require = require
+---@param modname string
+---@return any
+function require(modname)
+	return dofile_once(("mods/%s/lib/ecs/%s.lua"):format(whoami, (modname:gsub("%.", "/"))))
+end
 
----@class (exact) ECS
----@field me Entity
----@field load fun(file: string, x: number?, y: number?, rotation: number?, scale_x: number?, scale_y: number?): Entity
+local ECS = require "src.ECS"
+require = _require
 
----@type ECS
----@diagnostic disable-next-line: missing-fields
-local M = {}
-
----@type table<string, (fun(): any) | (fun(...): any)[]>
-local index = {
-	me = function()
-		return Entity.from_id(GetUpdatedEntityID())
-	end,
-	load = {
-		function(filename, x, y, rotation, scale_x, scale_y)
-			filename = typed.must(filename, "string")
-			x = typed.maybe(x, 0)
-			y = typed.maybe(y, 0)
-			rotation = typed.maybe(rotation, 0)
-			scale_x = typed.maybe(scale_x, 1)
-			scale_y = typed.maybe(scale_y, 1)
-			local eid = EntityLoad(filename, x, y)
-			-- EntityLoad truncates
-			EntitySetTransform(eid, x, y, rotation, scale_x, scale_y)
-			return Entity.from_id(eid)
-		end,
-	},
-}
-
-local mt = metatable.metatable(index, {}, "ECS")
-
-freeze.freeze(M, "ECS", mt)
-return M
+return ECS
